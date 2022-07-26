83b8aff7a19d872702f8df1c032bcd3abcad34ec
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-07-26 10:21:55 -0300

    create specific model for api and update Carrera Controller for test

--- backend/basic/modules/apiv1/controllers/CarreraController.php
+++ backend/basic/modules/apiv1/controllers/CarreraController.php
@@ -5,9 +5,9 @@ namespace app\modules\apiv1\controllers;
 use yii\rest\ActiveController;
 
 /**
- * Default controller for the `apiv1` module
+ * Carrera controller for the `apiv1` module
  */
 class CarreraController extends ActiveController
 {
-    public $modelClass = 'app\models\Carrera';
+    public $modelClass = 'app\modules\apiv1\models\Carrera';
 }
