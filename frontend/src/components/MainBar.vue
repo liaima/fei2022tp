9fde8535f2c55aa3d4c9c3a1a0e423ca2ba37185
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-07-30 23:47:05 -0300

    Visual mask

---
+++ frontend/src/components/MainBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-app-bar
+      app
+      color="#003467"
+      elevation="4"
+      dark
+      >
+      <v-app-bar-nav-icon @click="$emit('toggle-menu')"></v-app-bar-nav-icon>
+      <v-app-bar-title>Aulas Curza</v-app-bar-title>
+      <v-spacer></v-spacer>
+     <v-btn small text fab @click="logout">
+        <v-icon>mdi-exit-to-app</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'MainBar',
+
+    data: () => ({
+
+    }),
+    methods: {
+      logout(){
+        console.log('logout....')
+      }
+    }
+  }
+</script>
---
+++ frontend/src/components/MainMenu.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-container>
+    <v-navigation-drawer
+      color="#67b4ff"
+      app
+      permanent
+      >
+      <v-list-item>
+        <v-img
+          alt="Aulas Logo"
+          class="shrink mr-2"
+          contain
+          :src="require('../assets/logo_blanco.png')"
+          transition="scale-transition"
+          width="70"
+          />
+          <v-list-item-content>
+            <v-list-item-title class="text-h6">
+              Aulas Curza
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              FEI
+            </v-list-item-subtitle>
+          </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list danse>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+          >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'MainMenu',
+
+    data: () => ({
+      items: [
+        {title: 'Home', icon: 'mdi-view-dashboard'},
+        {title: 'Acerca de', icon: 'mdi-information-variant'}
+      ],
+    }),
+    mouted(){
+
+    },
+    computed: {
+
+    }
+  }
+</script>
