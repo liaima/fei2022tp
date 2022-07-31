7137193f566a3a50f8eb7de4249f45370e68617d
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-07-31 18:08:04 -0300

    change crud for tabe view

--- frontend/src/plugins/router.js
+++ frontend/src/plugins/router.js
@@ -4,6 +4,7 @@ import AulasHome from '@/views/AulasHome'
 import AulasAbout from '@/views/AulasAbout'
 import AulasTools from '@/views/AulasTools'
 import CourseCrud from '@/views/course/CourseCrud'
+import AddCourse from '@/views/course/AddCourse'
 import NotFound from '@/views/NotFound.vue'
 Vue.use(VueRouter)
 
@@ -12,6 +13,7 @@ const routes = [
   { path: '/about', name: 'about', component: AulasAbout },
   { path: '/tools', name: 'tools', component: AulasTools },
   { path: '/tools/course', name: 'course', component: CourseCrud },
+  { path: '/tools/course/add_course', name: 'addCourse', component: AddCourse },
   { path: '/404/', component: NotFound },
   { path: '*', redirect: '/404' }
 
--- frontend/src/views/course/CourseCrud.vue
+++ frontend/src/views/course/CourseCrud.vue
@@ -1,12 +1,44 @@
 <template>
   <div>  
     <h1>Administrar Carrera</h1>
-    <router-link :to='{name:"addCourse"}'>+</router-link>
-    <v-text-field
-      v-model="reg.name"
-      label="Nombre"
-      required
-      ></v-text-field>
+    <div>
+     <router-link :to='{name:"addCourse"}'>
+       <v-btn
+       tile
+       color="success"
+       >
+         <v-icon left>
+           mdi-plus
+         </v-icon>
+         Nueva
+       </v-btn>
+     </router-link>
+    </div>
+    <div>
+      <v-simple-table>
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th class="text-left">
+                ID
+              </th>
+              <th class="text-left">
+                Nombre
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="course in courses"
+              :key="course.id"
+            >
+              <td>{{ course.id }}</td>
+              <td>{{ course.nombre }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </div>
   </div>
 </template>
 
@@ -24,17 +56,20 @@ export default {
     pageCount: 0,
     totalCount: 0,
     loading: false,
+    courses: [],
   }),
   methods: {
     loadData(page){
       this.loading = true;
       var that = this;
-      this.axios.get('/apiv1/carrera?per-page=1@page=' + page)
+      this.axios.get('/apiv1/carrera?per-page=10@page=' + page)
         .then( (response) => {
           console.log(response)
           if (response.data.length > 0) {
+            that.courses = response.data
+            /*
             that.reg.id = response.data[0].id
-            that.reg.name = response.data[0].nombre
+            that.reg.name = response.data[0].nombre*/
             that.currentPage = response.headers["x-pagination-current-page"]
             that.pageCount = response.headers["x-pagination-page-count"]
             that.totalCount = response.headers["x-pagination-total-count"]
