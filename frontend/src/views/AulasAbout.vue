3f089fbc04728a5be27b42da8941c052ea3ebacb
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-08-01 15:37:47 -0300

    Change About

--- frontend/src/views/AulasAbout.vue
+++ frontend/src/views/AulasAbout.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1>Acerca de...</h1>
+  <div>
+    <h1>Acerca de...</h1>
+    <p>Aplicación para la administración de las reservas de las aulas del CURZA</p>
+    <v-icon>mdi-github</v-icon><a href="https://github.com/liaima/fei2022tp.git" target="_blank">Repositorio Git Hub</a>
+  </div>
 </template>
 
 <script>
