66fcb74b516c89f81908a2dc000524a0f35a53a9
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-08-01 12:31:46 -0300

    add link to home on 404

--- frontend/src/views/NotFound.vue
+++ frontend/src/views/NotFound.vue
@@ -1,21 +1,13 @@
 <template>
   <div>
-    <h1>Error 404 - Pagina No Econtrada</h1>
-    <p>{{ route }}</p>
+    <h1>Error 404 - Página No Econtrada</h1>
+    <p>No se encontro la página</p>
+    <router-link to='/'>Ir a Home</router-link>
   </div>
 </template>
 
 <script>
 export default {
   name: 'NotFound',
-  computed: {
-    route(){
-      console.log(this.$route.path)
-      if(this.$route.params.route){
-        return this.$route.params.route
-      }
-      return this.$route.path 
-    }
-  }
 }
 </script>
