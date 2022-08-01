b587e882f3e52fca7b2886a72bf7eb7161fcf167
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-08-01 15:47:39 -0300

    cange loader of reservation and schedule

--- frontend/src/views/reservation/ReservationCrud.vue
+++ frontend/src/views/reservation/ReservationCrud.vue
@@ -15,25 +15,12 @@
      </router-link>
     </div>
     <div>
-      <v-dialog
-      v-model="loading"
-      persistent
-      width="400"
-    >
-      <v-card
-        color="primary"
-        dark
-      >
-        <v-card-text>
-          Cargando...
-          <v-progress-linear
-            indeterminate
-            color="white"
-            class="mb-0"
-          ></v-progress-linear>
-        </v-card-text>
-      </v-card>
-    </v-dialog>
+    <v-progress-linear
+      indeterminate
+      color="teal"
+      :hidden="!loading"
+    ></v-progress-linear>
+
       <v-simple-table>
         <template v-slot:default>
           <thead>
